e3c8dbb9a529cd625550dd52f533baad038cdc63
servicioaguamilagro <124011461+servicioaguamilagro@users.noreply.github.com>
2023-10-31 21:21:36 -0500

    Interfaz 1 pantalla principal

---
+++ inicio/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def pagina_inicio(request):
+    return render(request, "paginainicio.html", )
+
+def pagina_inicio(request):
+    return render(request, "home.html", )
---
+++ static/js/carrusel.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay:3000,
+        pauseOnMouseEnter: true,
+        disableOnInteraction:false,
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
