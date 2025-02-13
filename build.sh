9cf54893a9b5babb974516f93a0d0441ebb4eac0
servicioaguamilagro <124011461+servicioaguamilagro@users.noreply.github.com>
2025-02-13 18:23:06 -0500

    creacion del superuser en build

--- build.sh
+++ build.sh
@@ -10,3 +10,18 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+# Crear superusuario automáticamente si no existe
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = "admin"
+email = "edhisson97sanmartin@gmail.com"
+password = "admin123"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print("Superusuario creado con éxito.")
+else:
+    print("El superusuario ya existe.")
+EOF
