546b6d7d47d278391c7d8c2608e9602931c0293a
servicioaguamilagro <124011461+servicioaguamilagro@users.noreply.github.com>
2025-02-18 20:35:30 -0500

    actualizacion Cloudinary

--- CarritoCompras/settings.py
+++ CarritoCompras/settings.py
@@ -26,6 +26,12 @@ CLOUDINARY_STORAGE = {
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+cloudinary.config( 
+  cloud_name = "dqphy48oi", 
+  api_key = "528441226936991", 
+  api_secret = "-xjPHIBDc09x32FSNq7T5uWblho"
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
