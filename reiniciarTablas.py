579f9c1aa959f96f0db0c7e83e6385f11de36aca
servicioaguamilagro <124011461+servicioaguamilagro@users.noreply.github.com>
2025-08-05 22:20:57 -0500

    reinicio de tablas y desactivar envio de mails 1.2

--- reiniciarTablas.py
+++ reiniciarTablas.py
@@ -1,25 +1,29 @@
 from django.db import connection
 from django.apps import apps
 
-# Lista de tus modelos
-models = [
-    'Caja',
-    'Factura',
-    'FacturaCredito',
-    'Servicio',
-    'PagoServicio',
-    'PagoServicioCombinado',
-]
+# Diccionario: app_name -> lista de modelos de esa app
+modelos_por_app = {
+    'operacion': [
+        'Caja',
+    ],
+    'ventas': [
+        'Factura',
+        'FacturaCredito',
+        'Servicio',
+        'PagoServicio',
+        'PagoServicioCombinado',
+    ],
+}
 
-# Elimina todos los registros y reinicia el ID
-for model_name in models:
-    model = apps.get_model('CarritoCompras', model_name)  # Reemplaza con el nombre real de tu app
-    model.objects.all().delete()
-    with connection.cursor() as cursor:
-        table_name = model._meta.db_table
-        if connection.vendor == 'sqlite':
-            cursor.execute(f'DELETE FROM sqlite_sequence WHERE name="{table_name}";')
-        elif connection.vendor == 'postgresql':
-            cursor.execute(f"ALTER SEQUENCE {table_name}_id_seq RESTART WITH 1;")
-        elif connection.vendor == 'mysql':
-            cursor.execute(f"ALTER TABLE {table_name} AUTO_INCREMENT = 1;")
+for app_name, modelos in modelos_por_app.items():
+    for model_name in modelos:
+        model = apps.get_model(app_name, model_name)
+        model.objects.all().delete()
+        with connection.cursor() as cursor:
+            table_name = model._meta.db_table
+            if connection.vendor == 'sqlite':
+                cursor.execute(f'DELETE FROM sqlite_sequence WHERE name="{table_name}";')
+            elif connection.vendor == 'postgresql':
+                cursor.execute(f"ALTER SEQUENCE {table_name}_id_seq RESTART WITH 1;")
+            elif connection.vendor == 'mysql':
+                cursor.execute(f"ALTER TABLE {table_name} AUTO_INCREMENT = 1;")
